dead082df408428813a09489322dc532c058ea76
Bhuvan-ops <bhuvan.k@robosoftin.com>
2025-01-08 18:40:40 +0530

    Update

--- routes/user_route.js
+++ routes/user_route.js
@@ -2,8 +2,26 @@ const express = require('express');
 const User = require('../models/user_model');
 const Review = require('../models/review_model');
 const STATUS_CODES = require("../constants.js");
+const authenticate = require("../middlewares/auth_middleware.js");
 
 const router = express.Router();
+
+router.get("/users", authenticate, async (req, res) => {
+  if (req.user.role !== "admin") {
+    return res
+      .status(STATUS_CODES.FORBIDDEN)
+      .json({ message: "Access forbidden: Admin role required" });
+  }
+
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error fetching users", error: err.message });
+  }
+});
 
 router.get('/profile/:userId', async (req, res) => {
   try {
